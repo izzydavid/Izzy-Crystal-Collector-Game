7ee7631e2405ac922606d57c2f05d44ec3fb7f02
IzzyDavid <davidirizarry@Davids-MacBook-Pro.local>
2019-10-13 18:30:27 -0400

    Final Crystal Collector Game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,240 +1,145 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('.carousel');
-  var instances = M.Carousel.init(elems, numVisible);
+$(document).ready(() => {
+
+$('.container, .instructions').hide(100);
+M.AutoInit();
+
+$("#playButton").on("click", function () {
+    $("#playButton").remove();
+    $(".instructions").delay(200).fadeIn(2000); 
 });
 
-numVisible = 5; 
-
-document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('.sidenav');
-  var instances = M.Sidenav.init(elems);
+$(".instructions").on("click", function () {
+    $(".instructions").remove();
+    $(".container").delay(200).fadeIn(2000); 
+    $('.carousel').delay(200).fadeIn(2000); 
+    $("#video").animate({
+    opacity: 0.8
+    });
 });
-// $('#playButton').hide();
-// $('strong').hide();
-// $('#blanks').hide();
-// $('#animeTitle').hide();
-// $('#animeTitle2').hide();
-// $('#myBtn').hide();
-// $('#myBtn2').hide();
-// $('.btn').hide();
-// $('h1').hide();
-// $('#animePick').hide();
+    
+    //program generates a random number between 19 and 120
+    let randNumber = Math.floor(Math.random() * ((120 - 19) + 1) + 19);
+    $('#randNum').html(randNumber);
 
-// $('#playButton').delay(1000).fadeIn('slow');
-
-
-// $("#playButton").on("click", function () {
-// $('#myBtn').delay(200).fadeIn('slow');
-// $('#myBtn2').delay(200).fadeIn('slow');
-// $('#animePick').delay(200).fadeIn('slow');
-// $('#playButton').remove();
-// $('#video').animate({
-//   opacity: 0.9,
-
-// })
-// });
-
-// $("#myBtn").on("click", function () {
-// $('strong').delay(200).fadeIn('slow');
-// $('#blanks').delay(200).fadeIn('slow');
-// $('#animeTitle').delay(200).fadeIn('slow');
-// $('#btn3').delay(200).fadeIn('slow');
-// $('#myBtn').remove();
-// $('#myBtn2').remove();
-// $('#animePick').remove();
-// $('#myBtn3').delay(200).fadeIn('slow');
-// $('#myBtn4').delay(200).fadeIn('slow');
-// $('#myBtn5').delay(200).fadeIn('slow');
-// $('#myBtn6').delay(200).fadeIn('slow');
-
-// });
-
-// $("#myBtn2").on("click", function () {
-// $('strong').delay(200).fadeIn('slow');
-// $('#blanks').delay(200).fadeIn('slow');
-// $('#animeTitle2').delay(200).fadeIn('slow');
-// $('#btn3').delay(200).fadeIn('slow');
-// $('#myBtn2').remove();
-// $('#myBtn').remove();
-// $('#animePick').remove();
-// $('#myBtn3').delay(200).fadeIn('slow');
-// $('#myBtn4').delay(200).fadeIn('slow');
-// $('#myBtn5').delay(200).fadeIn('slow');
-// $('#myBtn6').delay(200).fadeIn('slow');
-// });
+    //each crystal has a hidden value i.e. random number between1 and 12
+    let bluecrystal = Math.floor(Math.random() * 12) + 1;
+    let greencrystal = Math.floor(Math.random() * 12) + 1;
+    let redcrystal = Math.floor(Math.random() * 12) + 1;
+    let whitecrystal = Math.floor(Math.random() * 12) + 1;
+    let pinkcrystal = Math.floor(Math.random() * 12) + 1;
 
 
+    //player clicks the crystals to display value to totalscore
+    let wins = 0; //#wins
+    let losses = 0;  //#losses
 
+    let totalscore = 0;
+    $('#totalscore').text(totalscore);
 
-// var start = appStart({
-// 	getInitialState: function() {
-// 		return {
-// 			alphabet: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" , "@","-"],
-// 			words: '',
-// 			randomWord: '',
-// 			letters: [],
-// 			clickedLetters:[],
-// 			matchedLetters: [],
-// 			lives:6,
-// 			initialLives:6,
-// 			clickedButton:null,
-// 			gameStarted:false,
-// 			nextWord:false,
-// 			chosenLevel:'medium',
-// 			score:0,
-// 			gameOver:false
-// 		}
-// 	},
-// }); 
+    function win() {
+        wins = wins + 1;
+        $('#wins').text(wins);
+        resetTwo();
+        reset();
+        alert("You won this round!"); 
 
-// appStart()
+    }
 
-    var animeShows = ["death note", "monster", "rurouni kenshin", "pokemon", "trigun",
-      "naruto", "cowboy bepop", "evangelion", "dragon ball z", "flcl"
-    ];
+    function lose() {
+        losses = losses + 1;
+        $('#losses').text(losses);
+        resetTwo();
+        reset();
+        alert("You lost this round, but try again!"); 
+    }
 
-    var animeMovies = ["the wind rises", "grave of the fireflies", "my neighbor totoro", "princess mononoke", "ponya", "spirited away",
-      "castle in the sky", "akira", "ghost in the shell", "howl moving castle", "samurai x"
-    ];
+    $('#bluecrystal').click(() => {
+        totalscore = totalscore + bluecrystal; 
+        $('#totalscore').text(totalscore);
+        if (totalscore === randNumber) {
+            win();
+            reset();
+            resetTwo();
+        } else if (totalscore > randNumber) {
+            lose();
+            reset();
+            resetTwo();
+        }
+    });
 
-    var topics = ["cowboy bepop", "trigun", "my neighbor totoro", "death note"];
+    $('#greencrystal').click(() => {
+        totalscore = totalscore + greencrystal;
+        $('#totalscore').text(totalscore);
+        if (totalscore === randNumber) {
+            win();
+            reset();
+            resetTwo();
+        } else if (totalscore > randNumber) {
+            lose();
+            reset();
+            resetTwo();
+        }
+    });
 
+    $('#redcrystal').click(() => {
+        totalscore = totalscore + redcrystal;
+        $('#totalscore').text(totalscore);
+        if (totalscore === randNumber) {
+            win();
+            reset();
+            resetTwo();
+        } else if (totalscore > randNumber) {
+            lose();
+            reset();
+            resetTwo();
+        }
+    });
 
-    $("#myBtn3, #myBtn4, #myBtn5, #myBtn6").on("click", function intiialButtons() {
-      // Grabbing and storing the data-anime property value from the button
-      var anime = $(this).attr("data-name");
+    $('#whitecrystal').click(() => {
+        totalscore = totalscore + whitecrystal;
+        $('#totalscore').text(totalscore);
+        if (totalscore === randNumber) {
+            win();
+            reset();
+            resetTwo();
+        } else if (totalscore > randNumber) {
+            lose();
+            reset();
+            resetTwo();
+        }
+    });
 
-      // Constructing a queryURL using the anime name
-      var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-        anime + "&api_key=Qxpp4x5d7fMc17qfyggEeDXHcJFmzIWO&limit=36";
-
-      // Performing an AJAX request with the queryURL
-      $.ajax({
-          url: queryURL,
-          method: "GET"
-        })
-      // After data comes back from the request
-      .then(function (response) {
-        
-          console.log(queryURL);
-          console.log(response);
-
-      //Start of For Loop for images and pushing the Gify images into the materialize framework/cards and establishing the two different states of the images "Still" and "Animated" when someone enters and leaves the element//
-        var stillGify; 				
-        var activeGify;
-        for (var i = 0; i < 36; i++) {
-        console.log(response.data[i].images.original.url);
-        activeGify = response.data[i].images.original.url; 
-        stillGify = response.data[i].images.original.url;
-
-        // $('.card-image').hover(function (e) {
-        // 	$(this).attr("src", $(this).attr(activeGify))
-        // 		}, function (e) {
-        // 	$(this).attr("src", $(this).attr(stillGify))
-        // 	}
-        // 	);
-        $('.carousel').prepend(`
-        <a class = "carousel-item" href="JavaScript:Void(0)">
-        <img src = "${stillGify}">
-        <div class="card-reveal"> 
-						<span class="card-title activator grey-text text-darken-4" src = '${response.data[i].title}'><i class="material-icons right"></i></span>
-						<div class="card-action"> <a href="${response.data[i].url}" target="_blank">Click Here</a>
-						</div>
-						</div>			
-        </a> 
-        
-        `)
-      
-        M.AutoInit();
-
-      };
-      
-          // Start of MouseEnter and MouseLeave Enter Events//
-    // 	$(document).ready(function(){
-    // 		$(".card-image").find('img').mouseenter(function(){
-    // 		  if($("#imgAnimate").attr('src','form.jpg')){
-    // 			  $("#imgAnimate").attr('src','form.gif');
-    // 		  }
-    // 		  $(this).mouseleave(function(){
-    // 			  if($("#imgAnimate").attr('src','form.gif')){
-    // 			  $("#imgAnimate").attr('src','form.jpg');
-    // 		  }
-    // 		  });
-    // 		});
-    // 	  });
-
-
-    // End of MouseEnter and MouseLeave Enter Events
-
-
-
-        
-    //End of For Loop for images and pushing the Gify images into the materialize framework/cards and establishing the two different states of the images "Still" and "Animated" when someone enters and leaves the element//
-
+    $('#pinkcrystal').click(() => {
+        totalscore = totalscore + pinkcrystal;
+        $('#totalscore').text(totalscore);
+        if (totalscore === randNumber) {
+            win();
+            reset();
+            resetTwo();
+        } else if (totalscore > randNumber) {
+            lose();
+            reset();
+            resetTwo();
+        }
+    });
 
     
 
-  });
+    //random numbers (jewels) get added together: look above
+    //script determines if player wins or loses and outcome recorded in winsLosses
+    //game restarst, page does NOT reload
+    function reset() {
+        totalscore = 0;
+        $('#totalscore').text(totalscore);
+    }
+
+    function resetTwo() {
+        randNumber = Math.floor(Math.random() * ((120 - 19) + 1) + 19);
+        $('#randNum').html(randNumber);
+        bluecrystal = Math.floor(Math.random() * 12) + 1;
+        greencrystal = Math.floor(Math.random() * 12) + 1;
+        redcrystal = Math.floor(Math.random() * 12) + 1;
+        whitecrystal = Math.floor(Math.random() * 12) + 1;
+        pinkcrystal = Math.floor(Math.random() * 12) + 1;
+    }
 });
-
-
-
-const $root = document.querySelector("#root");
-
-let score;
-let targetScore;
-
-const makeGuess = () => {
-  const $score = document.querySelector("#root p");
-  $score.textContent = "Score: " + score + " | " + "Target: " + targetScore;
-
-  if (score > targetScore) {
-    alert("You lost this round!");
-    playRound();
-  } else if (score === targetScore) {
-    alert("You won this round!");
-    playRound();
-  }
-};
-
-const Crystal = function(color) {
-  this.element = document.createElement("div");
-  this.element.className = "crystal " + color;
-  this.value = 0;
-
-  this.element.addEventListener(
-    "click",
-    () => {
-      score += this.value;
-      makeGuess();
-    },
-    false
-  );
-};
-
-Crystal.prototype.render = function(target) {
-  this.value = Math.floor(Math.random() * 15) + 1;
-  target.appendChild(this.element);
-};
-
-const crystals = [
-  new Crystal("red"),
-  new Crystal("blue"),
-  new Crystal("green")
-];
-
-const playRound = () => {
-  const fragment = document.createDocumentFragment();
-  const $score = document.createElement("p");
-  targetScore = Math.floor(Math.random() * 50) + 25;
-  score = 0;
-  $score.textContent = "Score: " + score + " | " + "Target: " + targetScore;
-  crystals
-    .sort(() => 0.5 - Math.random())
-    .forEach(crystal => crystal.render(fragment));
-  fragment.appendChild($score);
-  $root.innerHTML = "";
-  $root.appendChild(fragment);
-};
-
-playRound();
